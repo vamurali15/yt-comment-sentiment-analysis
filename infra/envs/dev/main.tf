5dd4f16171f7ed62711a4f238f5defcaad5d2313
Balamurali Arumugam <va.balan.mca@gmail.com>
2025-10-22 12:16:55 +0000

    Initial commit

---
+++ infra/envs/dev/main.tf
@@ -0,0 +1,25 @@
+module "pubsub" {
+  source = "../../modules/pubsub"
+  region     = var.region
+  project_id = var.project_id
+}
+
+module "bigquery" {
+  source     = "../../modules/bigquery"
+  project_id = var.project_id
+  region     = var.region
+}
+
+module "storage" {
+  source     = "../../modules/storage"
+  project_id = var.project_id
+  region     = var.region
+}
+
+module "composer" {
+  source     = "../../modules/composer"
+  project_id = var.project_id
+  region     = var.region
+  name       = "yt-pipeline-composer"
+  cf_sa_email = var.cf_sa_email
+}
---
+++ infra/envs/dev/variables.tf
@@ -0,0 +1,11 @@
+variable "project_id" { 
+  default = "dna-poc-training" 
+} 
+
+variable "region" { 
+  default = "us-central1" 
+} 
+
+variable "cf_sa_email" { 
+  default = "517100079961-compute@developer.gserviceaccount.com" 
+} 
